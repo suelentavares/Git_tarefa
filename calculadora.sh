ad62814428fc79210df706c10da73e87974396e4
suelentavares <94622266+suelentavares@users.noreply.github.com>
2024-11-07 12:41:36 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -7,3 +7,9 @@ python3 /home/suelen/modulo1/python/calculadora.py
 
 # Para execultar a calculadora  abra o comando linuux ubuntu, depois colocar o diretório 
 
+ Expricação para o cógito python
+ 1- O código solicita ao usuário dois  valores.
+ 2- Os valres inseridos pelo usuário são  cnvertidos para números de pnto flutuante(float).
+ 3- O código realizar oprações matemáticos cm oos números ineridos pelo usuário, soma, subbtração, divisão e multiplicação
+ 4- O código mprime os  resultado das operações matemáticas no terminal.
+ 5- O código compara o primero número e depois perirar qual operações você digitar  e inserir o segundo número  e no final vai parecer o valor da operação.
