36105199ea8c0a83d676b173255753b59d43d076
suelentavares <94622266+suelentavares@users.noreply.github.com>
2024-11-07 12:37:25 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install python3
 
-python3 /home/suelen/modulo1/python/calculadora.p
+python3 /home/suelen/modulo1/python/calculadora.py
 
 # Para execultar a calculadora  abra o comando linuux ubuntu, depois colocar o diretório 
 
